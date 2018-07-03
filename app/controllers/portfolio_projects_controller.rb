9921d0212f03eb064c6a061a81557328b292d4f9
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-07-03 13:50:42 -0700

    Integrated New and Create Actions for Portfolio Projects

--- app/controllers/portfolio_projects_controller.rb
+++ app/controllers/portfolio_projects_controller.rb
@@ -2,4 +2,23 @@ class PortfolioProjectsController < ApplicationController
   def index
     @portfolio_items = PortfolioProject.all 
   end
+
+  def new
+    @portfolio_item = PortfolioProject.new 
+  end
+
+
+  # POST /portfolio_project
+  
+  def create
+    @portfolio_item = PortfolioProject.new(params.require(:portfolio_project).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_projects_path , notice: 'Your Portfolio Project was successfully added.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
